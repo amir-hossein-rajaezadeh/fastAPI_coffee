78e8d899cd8c5fada770c33030ac0fb6af920918
AmirHossein <73388470+amir-hossein-rajaezadeh@users.noreply.github.com>
2024-12-12 14:57:18 +0330

    updated

--- main.py
+++ main.py
@@ -199,4 +199,4 @@ def delete_coffee(coffee_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", reload=True, host="localhost", port=8513)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
